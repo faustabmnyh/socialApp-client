6075fea65f47e4dae4ea3f3cc3f7fca1ebfe1f91
faustabmnyh <faustaabimanyu@gmail.com>
2020-11-25 23:31:53 +0700

    change browserouter to hash

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import {
   createHttpLink,
   InMemoryCache,
 } from "@apollo/client";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
